ef25fedbce7d3831c214450ab240df4d872fa2e6
SVEngelgem <stijn.vanengelgem@hotmail.com>
2018-05-14 00:16:47 +0200

    update basis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule,  NO_ERRORS_SCHEMA  } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { SearchComponent } from './search/search.component';
+//routing
+import { RouterModule } from "@angular/router";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    SearchComponent
+
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent},
+      { path: 'search', component: SearchComponent},
+      //de routes hier plaatsen per module component
+      { path: '', redirectTo: 'home', pathMatch: 'full'}
+    ], { useHash: true })
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
